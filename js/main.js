c27a081429bf92d94c7ea88b49b299618058b4c2
hongphucdang1 <hpd.dang@gmail.com>
2022-11-05 14:20:27 -0400

    Update render link

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ document.querySelector('button').addEventListener('click', apiRequest)
 async function apiRequest(){
     const rapperName = document.querySelector('input').value
     try{
-        const response = await fetch(`localhost:8000/api/${rapperName}`)
+        const response = await fetch(`https://rap-names-api-47ce.onrender.com/api/${rapperName}`)
         const data = await response.json()
 
         console.log(data)
